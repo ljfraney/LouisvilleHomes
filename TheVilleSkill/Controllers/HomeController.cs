a49bc5839c233ce9f13dd4d5a647a69708737a61
Josh Franey <josh.franey@outlook.com>
2018-11-13 23:28:58 -0500

    Added Home View

---
+++ TheVilleSkill/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheVilleSkill.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
